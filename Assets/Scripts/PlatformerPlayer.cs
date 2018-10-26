f453318e120ab42e6668cbe56be8d18c1a93ca38
jgch88 <radium.laboratories@gmail.com>
2018-10-26 19:31:06 +0800

    done section 6.2.1, 6.2.2

---
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerPlayer : MonoBehaviour {
+	public float speed = 250.0f;
+
+	private Rigidbody2D _body;
+
+	void Start () {
+		_body = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		float deltaX = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		Vector2 movement = new Vector2 (deltaX, _body.velocity.y);
+		_body.velocity = movement;
+	}
+}
